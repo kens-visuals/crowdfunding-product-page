c96cd7dda336179124f71ead8e78fd6a7a7806f4
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-11-21 15:55:22 -0800

    add bookmark btn toggle

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const nav = document.querySelector('.js-nav');
+const bookmarkBtn = document.querySelector('.js-bookmark');
 const hamburgerBtn = document.querySelector('.js-hamburger');
 const hamburgerIcon = document.querySelector('.js-hamburger-icon');
 
-hamburgerBtn.addEventListener('click', () => {
+const toggleNav = function () {
   if (!nav.classList.contains('open-nav')) {
     nav.classList.add('open-nav');
     hamburgerIcon.src = '../images/icon-close-menu.svg';
@@ -10,4 +11,18 @@ hamburgerBtn.addEventListener('click', () => {
     nav.classList.remove('open-nav');
     hamburgerIcon.src = '../images/icon-hamburger.svg';
   }
+};
+
+bookmarkBtn.addEventListener('click', () => {
+  if (!bookmarkBtn.classList.contains('bookmarked')) {
+    bookmarkBtn.classList.add('bookmarked');
+    bookmarkBtn.innerHTML = `<img src="./images/icon-bookmark.svg"
+    alt="" aria-hidden="true" class="crowdfund__bookmark" /> Bookmarked`;
+  } else {
+    bookmarkBtn.classList.remove('bookmarked');
+    bookmarkBtn.innerHTML = `<img src="./images/icon-bookmark.svg"
+    alt="" aria-hidden="true" class="crowdfund__bookmark" /> Bookmark`;
+  }
 });
+
+hamburgerBtn.addEventListener('click', toggleNav);

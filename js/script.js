6c6dc5c6e8eb950be706b165c819d546e7445631
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-11-21 22:53:04 -0800

    add radio button interactions and input modals

--- js/script.js
+++ js/script.js
@@ -1,79 +1,84 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable function-paren-newline */
 /* eslint-disable comma-dangle */
 /* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable func-names */
+
 const nav = document.querySelector('.js-nav');
 const bookmarkBtn = document.querySelector('.js-bookmark');
 const hamburgerBtn = document.querySelector('.js-hamburger');
-const selectBtns = document.querySelectorAll('.js-select');
 const modalCloseBtn = document.querySelector('.js-modal-close');
+
 const modalBg = document.querySelector('.js-modal-bg');
-const modals = document.querySelectorAll('.js-modal');
 const wrapperFlex = document.querySelector('.js-wrapper-flex');
+
+const modals = document.querySelectorAll('.js-modal');
+const figures = document.querySelectorAll('.js-figure');
+const selectBtns = document.querySelectorAll('.js-select');
 const checkboxes = document.querySelectorAll('.js-checkbox');
 const checkedBlocks = document.querySelectorAll('.js-checked');
 
 const [modalLarge, modalSmall] = modals;
-const [checkOne, checkTwo] = checkboxes;
-const [blockOne, blockTwo] = checkedBlocks;
 
-console.log(checkOne, checkTwo);
-console.log(blockOne, blockTwo);
+const imgPath = (imgName, className) =>
+  `<img src="./images/${imgName}.svg" alt="" aria-hidden="true" class="crowdfund__${className}" />`;
+
+const classRemover = (items, className) =>
+  items.forEach((item) => item.classList.remove(className));
 
 const toggleNav = function () {
   if (!nav.classList.contains('open-nav')) {
     nav.classList.add('open-nav');
-    hamburgerBtn.innerHTML = `
-    <img 
-        src="./images/icon-close-menu.svg"
-        alt="" 
-        aria-hidden="true"
-        class="crowdfund__hamburger" 
-    />`;
+    hamburgerBtn.innerHTML = imgPath('icon-close-menu', 'hamburger');
   } else {
     nav.classList.remove('open-nav');
-    hamburgerBtn.innerHTML = `
-    <img 
-        src="./images/icon-hamburger.svg"
-        alt="" aria-hidden="true"
-        class="crowdfund__hamburger" 
-    />`;
+    hamburgerBtn.innerHTML = imgPath('icon-hamburger', 'hamburger');
   }
 };
 
 const toggleBookamrkBtn = function () {
   if (!bookmarkBtn.classList.contains('bookmarked')) {
     bookmarkBtn.classList.add('bookmarked');
-    bookmarkBtn.innerHTML = `
-    <img 
-        src="./images/icon-bookmark.svg"
-        alt="" 
-        aria-hidden="true" 
-        class="crowdfund__bookmark" 
-    /> Bookmarked`;
+    bookmarkBtn.innerHTML = `${imgPath(
+      'icon-bookmark',
+      'bookmark'
+    )} Bookmarked`;
   } else {
     bookmarkBtn.classList.remove('bookmarked');
-    bookmarkBtn.innerHTML = `
-    <img 
-        src="./images/icon-bookmark.svg"
-        alt="" 
-        aria-hidden="true" 
-        class="crowdfund__bookmark" 
-    /> Bookmark`;
+    bookmarkBtn.innerHTML = `${imgPath('icon-bookmark', 'bookmark')} Bookmark`;
   }
 };
 
+const toggleFigureStyles = function (e) {
+  const target = e.target.closest('.js-figure');
+
+  classRemover(figures, 'active-grid');
+  classRemover(checkedBlocks, 'is-checked');
+
+  target.classList.add('active-grid');
+  target.lastElementChild.classList.add('is-checked');
+};
+
+hamburgerBtn.addEventListener('click', toggleNav);
+bookmarkBtn.addEventListener('click', toggleBookamrkBtn);
+
 selectBtns.forEach((btn) =>
   btn.addEventListener('click', () => {
-    modalBg.classList.add('is-visible');
-    modalLarge.classList.add('is-visible');
+    // eslint-disable-next-line no-return-assign
+    checkboxes.forEach((checkbox) => (checkbox.checked = false));
+    classRemover(figures, 'active-grid');
+    classRemover(checkedBlocks, 'is-checked');
+
+    modalBg.classList.add('is-modal-visible');
+    modalLarge.classList.add('is-modal-visible');
   })
 );
 
 modalCloseBtn.addEventListener('click', () => {
-  modalBg.classList.remove('is-visible');
-  modalLarge.classList.remove('is-visible');
+  modalBg.classList.remove('is-modal-visible');
+  modalLarge.classList.remove('is-modal-visible');
 });
 
-hamburgerBtn.addEventListener('click', toggleNav);
-bookmarkBtn.addEventListener('click', toggleBookamrkBtn);
+checkboxes.forEach((checkbox) =>
+  checkbox.addEventListener('click', toggleFigureStyles)
+);
